ef927f381d6d53c4c44c6d4a25f523b90d369c2e
Naol Sisay <naolasis02@gmil.com>
2024-05-08 10:59:48 +0300

    add project to the repository

---
+++ main.js
@@ -0,0 +1,5 @@
+const navToggle = document.querySelector('.nav-toggle');
+const nav = document.querySelector('.nav');
+navToggle.addEventListener('click', () => {
+    nav.classList.toggle('nav--visible');
+})
